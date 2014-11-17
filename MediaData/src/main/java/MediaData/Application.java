249857a2c630ab1287ef6c136b3a578c217f2a9d
janhway <janhway@gmail.com>
2014-11-17 23:07:43 +0800

    intial codes.

--- MediaData/src/main/java/MediaData/Application.java
+++ MediaData/src/main/java/MediaData/Application.java
@@ -1,5 +1,6 @@
 package MediaData;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
 }
