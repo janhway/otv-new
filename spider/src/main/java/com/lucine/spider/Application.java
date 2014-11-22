876c656a621194f36e9e3c39e5a309e4e6dc6f7d
janhway <janhway@gmail.com>
2014-11-22 11:43:12 +0800

    readme

--- spider/src/main/java/com/lucine/spider/Application.java
+++ spider/src/main/java/com/lucine/spider/Application.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) {
+    	
+    	// 修改为创建和调用爬虫任务. do it later.
         SpringApplication.run(Application.class, args);
     }
 }
