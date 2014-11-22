bfe64a98552675e8498a0210a45d56659ec6d365
janhway <janhway@gmail.com>
2014-11-22 12:10:46 +0800

    使用restTemplate

--- spider/src/main/java/com/lucine/spider/iqiyi/HttpPipeline.java
+++ spider/src/main/java/com/lucine/spider/iqiyi/HttpPipeline.java
@@ -4,9 +4,9 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
 
 import com.lucine.spider.entity.OttMedia;
-import com.lucine.spider.utils.Utils;
 
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
@@ -17,6 +17,8 @@ public class HttpPipeline implements Pipeline {
 	private final Logger log = LoggerFactory.getLogger(HttpPipeline.class);
 	
 	final String url = "http://127.0.0.1:8080/spider/ottmedia";
+	
+	final  RestTemplate restTemplate = new RestTemplate();
 
 	public void process(ResultItems resultItems, Task task) {
 
@@ -28,17 +30,20 @@ public class HttpPipeline implements Pipeline {
 		for(Map.Entry<String, Object> entry:all.entrySet()) {
 			
 			OttMedia om = (OttMedia) entry.getValue();
-			try {
-				log.info(om.toString());
-				byte[] bytesOM = om.toString().getBytes("utf-8");
-				
-				Utils.httpPost(bytesOM);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 			
+			String ret = restTemplate.postForObject(url, om, String.class);
+			
+			log.info("ret="+ret);
+			
+//			try {
+//				log.info(om.toString());
+//				byte[] bytesOM = om.toString().getBytes("utf-8");
+//				
+//				Utils.httpPost(bytesOM);
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}			
 		}
-
 	}
 
 }
